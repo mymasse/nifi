edbc6f1199ba0244f8d1e5e24fe6470403be03be
Mathieu Masse <mathieu.masse@gmail.com>
2016-11-24 15:12:17 -0500

    Add Spring Boot nifi test processor

---
+++ nifi-boot/src/main/java/com/mymasse/nifi/NifiBootApplication.java
@@ -0,0 +1,38 @@
+package com.mymasse.nifi;
+
+import org.apache.nifi.spring.SpringNiFiConstants;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
+import org.springframework.messaging.MessageChannel;
+
+@SpringBootApplication
+@EnableIntegration
+public class NifiBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NifiBootApplication.class, args);
+    }
+
+    @Bean(name = SpringNiFiConstants.FROM_NIFI)
+    public MessageChannel fromNiFi() {
+        return MessageChannels.direct().get();
+    }
+
+    @Bean(name = SpringNiFiConstants.TO_NIFI)
+    public MessageChannel toNiFi() {
+        return MessageChannels.queue().get();
+    }
+
+    @Bean
+    public IntegrationFlow mainFlow() {
+        return IntegrationFlows.from(fromNiFi())
+                .enrichHeaders(h -> h.header("NiFiContext", "Yes it works"))
+                .channel(toNiFi())
+                .get();
+    }
+}
---
+++ nifi-boot/src/test/java/com/mymasse/nifi/NifiBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mymasse.nifi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NifiBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
